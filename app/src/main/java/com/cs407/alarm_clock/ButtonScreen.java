70f7888339ddbdb4ea0ad0c77076664dac2bcd14
je-woodruff <jeremywoodruff1212@gmail.com>
2023-12-03 12:10:21 -0600

    Untested port of Jeremiah's Result Screen

--- app/src/main/java/com/cs407/alarm_clock/ButtonScreen.java
+++ app/src/main/java/com/cs407/alarm_clock/ButtonScreen.java
@@ -2,7 +2,9 @@ package com.cs407.alarm_clock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ButtonScreen extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class ButtonScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_screen);
+
+        TextView questionSpace = (TextView) findViewById(R.id.dragQuestion);
+
+
+    }
+
+    public void goToResults(String result) {
+        Intent intent = new Intent(this, ResultScreen.class);
+        intent.putExtra("result", result);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
