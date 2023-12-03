57ebb603dfd39d289f202c446abce24a87ac9292
je-woodruff <jeremywoodruff1212@gmail.com>
2023-12-03 12:26:00 -0600

    Untested port of Jeremiah's Button Screen

--- app/src/main/java/com/cs407/alarm_clock/ButtonScreen.java
+++ app/src/main/java/com/cs407/alarm_clock/ButtonScreen.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ButtonScreen extends AppCompatActivity {
 
@@ -14,6 +19,87 @@ public class ButtonScreen extends AppCompatActivity {
         setContentView(R.layout.activity_button_screen);
 
         TextView questionSpace = (TextView) findViewById(R.id.dragQuestion);
+        Intent oldIntent = getIntent();
+        String question = oldIntent.getStringExtra("q");
+        questionSpace.setText(question);
+        int answer = Integer.parseInt(oldIntent.getStringExtra("ans"));
+        int fakeAnswer1 = Integer.parseInt(oldIntent.getStringExtra("f1"));
+        int fakeAnswer2 = Integer.parseInt(oldIntent.getStringExtra("f2"));
+        int fakeAnswer3 = Integer.parseInt(oldIntent.getStringExtra("f3"));
+
+        ArrayList<String> options = new ArrayList<String>();
+        options.add(Integer.toString(answer));
+        options.add(Integer.toString(fakeAnswer1));
+        options.add(Integer.toString(fakeAnswer2));
+        options.add(Integer.toString(fakeAnswer3));
+        //shuffle the arraylist
+        Collections.shuffle(options);
+
+        Button option1 = (Button) findViewById(R.id.option1);
+        option1.setText(options.get(0));
+        Button option2 = (Button) findViewById(R.id.option2);
+        option2.setText(options.get(1));
+        Button option3 = (Button) findViewById(R.id.option3);
+        option3.setText(options.get(2));
+        Button option4 = (Button) findViewById(R.id.option4);
+        option4.setText(options.get(3));
+
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(options.get(0)) == answer) {
+                    goToResults("Correct");
+                } else {
+                    goToResults("Incorrect\n" + question + " is\n" + Integer.toString(answer));
+                }
+            }
+        });
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(options.get(1)) == answer) {
+                    goToResults("Correct");
+                } else {
+                    goToResults("Incorrect\n" + question + " is\n" + Integer.toString(answer));
+                }
+            }
+        });
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(options.get(2)) == answer) {
+                    goToResults("Correct");
+                } else {
+                    goToResults("Incorrect\n" + question + " is\n" + Integer.toString(answer));
+                }
+            }
+        });
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(options.get(3)) == answer) {
+                    goToResults("Correct");
+                } else {
+                    goToResults("Incorrect\n" + question + " is\n" + Integer.toString(answer));
+                }
+            }
+        });
+
+        Button skipButton = (Button) findViewById(R.id.skipButton);
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToResults("Skipped");
+            }
+        });
+
+        Button switchButton = (Button) findViewById(R.id.switchButton);
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToShake();
+            }
+        });
 
 
     }
