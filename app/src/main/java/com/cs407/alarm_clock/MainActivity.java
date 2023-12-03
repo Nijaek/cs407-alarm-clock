fa18e57357d108340d60c08601393ceb06995241
je-woodruff <jeremywoodruff1212@gmail.com>
2023-12-03 12:56:57 -0600

    Changes worked, removing temporary changes made to ActivityMain

--- app/src/main/java/com/cs407/alarm_clock/MainActivity.java
+++ app/src/main/java/com/cs407/alarm_clock/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.buttonTest);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToMath();
-            }
-        });
-    }
-    public void goToMath() {
-        Intent intent = new Intent(this, MathGenerator.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
     }
 }
