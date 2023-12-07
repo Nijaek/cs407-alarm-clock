1ff99ef497c275e3cdc1a76e5936d9b196fc2ee7
Nijaek <59636269+Nijaek@users.noreply.github.com>
2023-12-06 18:26:27 -0600

    Update alarm_overview.java

--- app/src/main/java/com/cs407/alarm_clock/alarm_overview.java
+++ app/src/main/java/com/cs407/alarm_clock/alarm_overview.java
@@ -1,7 +1,10 @@
 package com.cs407.alarm_clock;
 
 import android.util.Log;
+import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,12 +18,26 @@ public class alarm_overview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_overview);
 
+        // Get alarms
+        List<Alarm> alarms;
+        AlarmDataSource alarmDataSource = new AlarmDataSource(alarm_overview.this);
+        alarmDataSource.open();
+        alarms = alarmDataSource.getAllAlarms();
+        alarmDataSource.close();
+
+        // Set up container
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewAlarms);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        AlarmAdapter adapter = new AlarmAdapter(this, alarms);
+        recyclerView.setAdapter(adapter);
+
+        // Handle new button click
         buttonNewAlarm = findViewById(R.id.buttonNewAlarm);
         buttonNewAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(alarm_overview.this, alarm_selection.class);
-
                 startActivity(intent);
             }
         });
