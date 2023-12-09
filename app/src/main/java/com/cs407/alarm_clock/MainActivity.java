6241da8a3359894964ad14dc0187343650d43342
Nijaek <59636269+Nijaek@users.noreply.github.com>
2023-12-09 17:25:08 -0600

    Navigation menu

--- app/src/main/java/com/cs407/alarm_clock/MainActivity.java
+++ app/src/main/java/com/cs407/alarm_clock/MainActivity.java
@@ -3,11 +3,15 @@ package com.cs407.alarm_clock;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.annotation.NonNull;
+import android.view.Menu;
+import android.view.MenuInflater;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button buttonAlarm;
 
     @Override
@@ -15,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up the Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set-up button
         buttonAlarm = findViewById(R.id.buttonAlarm);
         buttonAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +33,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_my_alarms) {
+            // Handle My Alarms
+            startActivity(new Intent(this, alarm_selection.class));
+            return true;
+        } else if (itemId == R.id.action_create_question) {
+            // Handle Create a Question
+            return true;
+        } else if (itemId == R.id.action_settings) {
+            // Handle Settings
+            startActivity(new Intent(this, SettingsScreen.class));
+            return true;
+        }
+        return false;
     }
 }
