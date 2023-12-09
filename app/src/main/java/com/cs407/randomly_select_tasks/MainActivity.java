17dcd427f823d5ed0ee05ce65bcfa5373a027e6d
haiyih <hhe96@wisc.edu>
2023-12-08 23:59:48 -0600

    Update OnCreate Method

--- app/src/main/java/com/cs407/randomly_select_tasks/MainActivity.java
+++ app/src/main/java/com/cs407/randomly_select_tasks/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cs407.randomly_select_tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Button firstFragmentButton = findViewById(R.id.llCenter);
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.llCenter, llTop.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing First")
+                        .commit();
+            }
+        });
+
+        Button secondFragmentButoon = findViewById(R.id.llTop);
+        secondFragmentButoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.llCenter, llCenter.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing First")
+                        .commit();
+            }
+        });
     }
 }
