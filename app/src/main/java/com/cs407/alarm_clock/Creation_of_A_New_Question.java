f0cdecc918de6482bfe65a3d1283c5037400ac58
haiyih <hhe96@wisc.edu>
2023-12-10 16:27:54 -0600

    Commit again

---
+++ app/src/main/java/com/cs407/alarm_clock/Creation_of_A_New_Question.java
@@ -0,0 +1,16 @@
+package com.cs407.alarm_clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cs407.update_create_a_new_question.R;
+
+public class Creation_of_A_New_Question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_of_anew_question);
+    }
+}
